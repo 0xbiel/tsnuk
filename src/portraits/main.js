c756efcfd66e7c7f041e3da66f68d57ee20bd444
Biel <bielp07@gmail.com>
2021-05-30 09:57:49 -0300

    Load Image.
    Use local copy of p5.js, load image that you serve as a base to the
    artwork and set the canvas size to behave based on the image's dimensions.

--- src/portraits/main.js
+++ src/portraits/main.js
@@ -1,7 +1,17 @@
+let pic
+let centeredCanvas
+
+function preload() {
+    pic = loadImage('assets/me.png')
+}
+
 function setup() {
-    createCanvas(800, 800);
+    centeredCanvas = createCanvas(pic.width, pic.height)
+    let centerX = (windowWidth - pic.width) / 2
+    let centerY = (windowHeight - pic.height) / 2
+    centeredCanvas.position(centerX, centerY)
 }
 
 function draw() {
-    background(220);
+    background(220)
 }
