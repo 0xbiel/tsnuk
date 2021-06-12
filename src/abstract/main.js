7db7870a2ad42e01eda4c17ed7ca2427e9ff8373
Biel <bielp07@gmail.com>
2021-06-12 10:21:40 -0300

    Start abstract art.

---
+++ src/abstract/main.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(600, 600)
+}
+
+function draw() {
+    background(0)
+    ellipse(50, 50, 80, 80)
+    ellipse(278, 200, 400, 400)
+    ellipse(120, 100, 110, 110)
+    ellipse(430, 60, 18, 18)
+}
